906d58963d44985717dc165c73261316e25fc395
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2025-10-28 03:45:28 -0600

    Tercer proyecto a crear

---
+++ BootstrapWebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoostrapWebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
