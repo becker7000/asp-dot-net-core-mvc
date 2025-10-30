5aa34437a8fabc85f588e3543bb04baeb4309957
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2025-10-30 09:04:17 -0600

    EF Core

---
+++ Taskify/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskify.Entities; // Usando EF Core
+
+namespace Taskify
+{
+    // Configuraciones de la base de datos, tablas y campos (queries)
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Con esto indicamos que Tarea es una Entidad (es como suscribirla)
+        public DbSet<Tarea> Tareas { get; set; }
+
+        // Una migración es un paso intemedio 
+
+    }
+}
---
+++ Taskify/Entities/Tarea.cs
@@ -0,0 +1,14 @@
+﻿namespace Taskify.Entities
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Orden { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        // Se va a convertir en una Entity cuando
+        // la configuremos en EF Core Context
+    }
+}
